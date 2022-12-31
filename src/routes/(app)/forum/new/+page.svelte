58d6cf00581d90e205e061f6d148a5ff34617550
zrezke <jeretina.filip@gmail.com>
2022-12-31 20:37:46 +0100

    refactor new post page to use texteditor component

--- src/routes/(app)/forum/new/+page.svelte
+++ src/routes/(app)/forum/new/+page.svelte
@@ -2,10 +2,14 @@
 	import { page } from '$app/stores';
 	import RenderedPost from '$lib/RenderedPost.svelte';
 	import { POST as createPost } from '$lib/api/posts';
-	import { error } from '@sveltejs/kit';
-	import { marked } from 'marked';
+	import TextEditor from '$lib/TextEditor.svelte';
 	let title = '';
 	let content = '';
+
+	let createPostFields = {
+		title: '',
+		body: ''
+	};
 
 	function pasteHandler(e: ClipboardEvent) {
 		e.preventDefault();
@@ -18,89 +22,18 @@
 	<div class=" mt-10 flex w-3/4 mx-auto justify-center">
 		<div class="flex flex-col w-full justify-end">
 			<h3 class="text-3xl text-gray-300">Create a post</h3>
-			<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-				<ul
-					class="flex flex-wrap -mb-px text-sm font-medium text-center"
-					id="myTab"
-					data-tabs-toggle="#myTabContent"
-					role="tablist"
-				>
-					<li class="mr-2" role="presentation">
-						<button
-							class="inline-block p-4 rounded-t-lg border-b-2"
-							id="edit-tab"
-							data-tabs-target="#edit"
-							type="button"
-							role="tab"
-							aria-controls="edit"
-							aria-selected="false">Edit</button
-						>
-					</li>
-					<li class="mr-2" role="presentation">
-						<button
-							class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-							id="preview-tab"
-							data-tabs-target="#preview"
-							type="button"
-							role="tab"
-							aria-controls="preview"
-							aria-selected="false">Preview</button
-						>
-					</li>
-				</ul>
-			</div>
-			<div id="myTabContent">
-				<div
-					class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-					id="edit"
-					role="tabpanel"
-					aria-labelledby="edit-tab"
-				>
-					<textarea
-						bind:value={title}
-						rows="1"
-						class="mt-5 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Title"
-						style="resize: none;"
-					/>
-					<textarea
-						bind:value={content}
-						on:paste={(e) => pasteHandler(e)}
-						id="post-content-md"
-						rows="4"
-						class="mt-5 min-h-[300px] block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Content of your post [Markdown]"
-					/>
-				</div>
-				<div
-					class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-					id="preview"
-					role="tabpanel"
-					aria-labelledby="preview-tab"
-				>
-					<RenderedPost markdown={content} />
-				</div>
-				<div class="flex justify-end">
-					<button
-						class="max-w-fit px-4 py-2 mt-3 float-right text-sm font-medium bg-blue-600 text-white rounded-full border-2 border-blue-600 transition-color duration-150 hover:bg-blue-900 hover:border-blue-900"
-						on:click={async () => {
-							createPost(
-								{
-									title: title,
-									body: content
-								},
-								$page.data.session
-							).then(() => {
-								console.log($page);
-								console.log($page.error);
-								// location.href = '/forum';
-							});
-						}}
-					>
-						Create post
-					</button>
-				</div>
-			</div>
+			<TextEditor
+				bind:data={createPostFields}
+				onSubmit={async () =>
+					await createPost(
+						{
+							title: createPostFields.title,
+							body: createPostFields.body
+						},
+						$page.data.session
+					)}
+				submitButtonText="Create post"
+			/>
 		</div>
 	</div>
 </div>
